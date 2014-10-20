1f4b1424ee9325205bb25263f2fb4e772efd74ba
enham <enham@redhat.com>
2014-10-20 11:59:18 -0400

    Parameterizing app name

--- init.sh
+++ init.sh
@@ -5,7 +5,8 @@ clear
 echo
 echo "##############################################################"
 echo "##                                                          ##"   
-echo "##  Setting up the RH Convergence OpenShift Demo            ##"
+echo "## Setting up the Participant Sign-in Sheet  OpenShift Demo ##"
+echo "##      App Name=$APP_NAME                                  ##"   
 echo "##                                                          ##"   
 echo "##  brought to you by Evong Nham                            ##"   
 echo "##                                                          ##"   
@@ -14,8 +15,6 @@ echo
 
 #create new rails project
 #rhc create-app $APP_NAME ruby-1.9 mysql-5.5
-
-#build ror app
 scl enable ruby193 "rails new $APP_NAME"
 cd $APP_NAME
 cp ../install/Gemfile .
@@ -24,12 +23,12 @@ cp ../install/database.yml config/
 cp ../install/deploy .openshift/action_hooks
 cp ../install/index.html.erb app/views/participants/
 cp ../install/application.html.erb app/views/layouts/
-cp ../install/routes.rb config/
+#cp ../install/routes.rb config/
+sed -i '2i\  root :to => "participants#index"' config/routes.rb
 rm -f public/index.html
-touch .openshift/markers/force_clean_build
+#touch .openshift/markers/force_clean_build
 scl enable ruby193 "bundle install"
-
-#push to openshift
 #git add .
 #git commit -m "Initial commit"
 #git push
+#echo "Complete... your app is now available."
